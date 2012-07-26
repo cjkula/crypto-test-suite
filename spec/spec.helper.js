0d9ba488f6d1c01bb195b207a14338558071f5f8
Christopher Kula <cjkula@dhcp-172-24-32-131.jnpr.net>
2012-07-26 15:09:17 -0700

    Add CommandLine globals and helper methods for asynchronous testing of CommandLine actions.

--- spec/spec.helper.js
+++ spec/spec.helper.js
@@ -1,4 +1,53 @@
-// // load the Crypto module (sets the Crypto global variable)
+// load the CommandLine module (sets the CommandLine global variable)
+require('../src/command_line.js');
+
+// load the Crypto module (sets the Crypto global variable)
 require('../src/crypto.js');
-// // add module to window object
-window = { Crypto: Crypto };
+
+// add modules to window object
+window = { 
+	CommandLine: CommandLine,
+	Crypto: Crypto 
+};
+
+//////////////////////////////////////////////////////////
+// helper methods for asynchronous command line processing
+//////////////////////////////////////////////////////////
+
+// ###### execWaitsRunsReply ######
+// asynchronously
+//    executes `command`, waits for completion,
+//    calls the callback, and waits for the reply callback.
+// All wrapped in Jasmine waitsFor / runs control blocks.
+execWaitsRunsReply = function(command, callback) {
+	var response, tagback;
+	runs(function() {
+		response = { completed: false };
+		CommandLine.exec(command, function(e, stdout) {
+			response.stdout = stdout;
+			response.completed = true;
+		});
+	});
+	waitsFor(function() { 
+		return response.completed;
+	});
+	runs(function() {
+		tagback = false;
+		callback(response.stdout, function() {
+			tagback = true;
+		});
+	});
+	waitsFor(function() { 
+		return tagback;
+	});
+}
+
+// ###### execWaitsRuns ######
+// same as execWaitsRunsReply except does not wait for reply from callback.
+execWaitsRuns = function(command, callback) {
+	execWaitsRunsReply(command, function (stdout, reply) {
+		callback(stdout);
+		reply();
+	});
+}
+
